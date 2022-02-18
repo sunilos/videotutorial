01c06bfe9bdf5f3272e486e99dec3be4f67a5031
SunilOS <sunilos007@gmail.com>
2022-02-18 08:04:15 +0530

    IO classes

--- IO/TestPrintWriter.java
+++ IO/TestPrintWriter.java
@@ -1,21 +1,31 @@
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
+/**
+ * It creates PrintWriter object that used to write data line by line
+ * 
+ * @author Sunil Sahu
+ *
+ */
 public class TestPrintWriter {
 
 	public static void main(String[] args) throws Exception {
 
 		FileWriter writer = new FileWriter("f:/newtest.txt");
+
+		// create PrintWriter fro writer object
 		PrintWriter pw = new PrintWriter(writer);
 
+		// Write file lines
 		for (int i = 0; i < 5; i++) {
 			pw.println(i + " : Line");
 		}
 
+		// Close streams
 		pw.close();
 		writer.close();
 
-		System.out.println("Check c:/newtest.txt");
+		System.out.println("Open c:/newtest.txt and see your data");
 
 	}
 
