d117519f1b4c0ba53559de0874fb6fe4d01f1555
SunilOS <sunilos007@gmail.com>
2022-02-18 08:01:16 +0530

    IO classes

--- IO/ReadObject.java
+++ IO/ReadObject.java
@@ -11,16 +11,22 @@ public class ReadObject {
 
 	public static void main(String[] args) throws Exception {
 
+		// Open a binary file
 		FileInputStream file = new FileInputStream("f:/object.ser");
+
+		// Read an object from file
 		ObjectInputStream in = new ObjectInputStream(file);
 
+		// Read object
 		Marksheet m = (Marksheet) in.readObject();
 
+		// Display data
 		System.out.println(m.name);
 		System.out.println(m.physics);
 		System.out.println(m.chemistry);
 		System.out.println(m.maths);
 
+		// Close the file
 		in.close();
 		file.close();
 	}
